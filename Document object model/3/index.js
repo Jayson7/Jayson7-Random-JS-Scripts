486a2b52eb10ee3b88ce460dd7d60bd7c91cc3b1
jayson7 <lexxiijoo70@gmail.com>
2021-12-23 12:22:29 +0100

    js

--- Document object model/3/index.js
+++ Document object model/3/index.js
@@ -2,3 +2,6 @@ window.oncontextmenu = (e) => {
     e.preventDefault();
     document.querySelector("body").innerHTML = "i did it, its disabled 🤣";
 };
+document.addEventListener("keyup", (e) => {
+    console.log(e.code);
+});
