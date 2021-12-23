ace882bc3c08e2e6e171be8b7f0e1252734cd310
jayson7 <lexxiijoo70@gmail.com>
2021-12-23 16:58:47 +0100

    js

--- Network detector/using webpage/index.js
+++ Network detector/using webpage/index.js
@@ -4,9 +4,17 @@ const speed = networkInformation.downlink;
 
 var infobox1 = document.querySelectorAll(".info-box div")[0];
 var infobox2 = document.querySelectorAll(".info-box div")[1];
+var infobox3 = document.querySelectorAll(".info-box div")[2];
+var infobox4 = document.querySelectorAll(".info-box div")[3];
+var infobox5 = document.querySelectorAll(".info-box div")[4];
 
 outputInfo = () => {
     infobox1.textContent = "Speed : " + speed;
+    infobox4.textContent = "SaveData : " + networkInformation.saveData;
+
+    infobox3.textContent = "RRT : " + networkInformation.rtt;
+    infobox5.textContent = "Online : " + window.navigator.onLine;
+
     infobox2.textContent =
         "Network Type : " + networkInformation["effectiveType"];
     console.log(networkInformation);
